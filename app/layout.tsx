e67f1601f5f011cd41270c74e2619d9fecba0509
sujin <sujin3473@gmail.com>
2024-02-04 23:26:37 +0900

    add: navigation 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex">
+        <div className="flex px-40 h-full">
           <Navigation />
           {children}
         </div>
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 const Navigation = () => {
   return (
-    <nav className="flex flex-col">
-      <p>{info.name}</p>
+    <nav className="flex flex-col py-40 justify-between text-[#1513f9] h-full">
+      <div>
+        <h1 className="text-[40px] font-bold">{info.name}</h1>
+      </div>
+      <div className="flex flex-col">
+        <Link href="about">about</Link>
+        <Link href="experience">experience</Link>
+        <Link href="projects">projects</Link>
+      </div>
     </nav>
   );
 };
