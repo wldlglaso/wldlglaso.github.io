13a12e9807b67826b7bcc6bbe04ef726a5de1f31
sujin <sujin3473@gmail.com>
2024-02-04 22:59:02 +0900

    initial commit

---
+++ components/navigation.tsx
@@ -0,0 +1,13 @@
+const Navigation = () => {
+  return (
+    <nav className="flex flex-col">
+      <p>{info.name}</p>
+    </nav>
+  );
+};
+
+const info = {
+  name: "Sienna Yim",
+};
+
+export default Navigation;
