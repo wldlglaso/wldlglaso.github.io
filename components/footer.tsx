4d3acef0d5fa31cd44625212ca296607acf9bd1c
sujin <sujin3473@gmail.com>
2024-03-21 15:47:25 +0900

    add: Footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,10 @@
 const Footer = () => (
-  <section>
-    <p className="text-gray-A mt-[60px]">
-      친절하고 매력적인 디지털 경험을 전달하는 프론트엔드 개발자 입니다.
+  <section className="text-gray-A leading-[28px] text-[16px]">
+    <p className="mt-[60px]">
+      친절하고 매력적인 디지털 경험을 전달하는 디자이너 입니다.
     </p>
-    <p className="text-blue-6A">임수진 | Sienna Yim | sujin3473@gmail.com</p>
-    <p className="text-blue-D mt-[20px]">
-      Cpoyright 2024. Sujin Yim. All rights reserved.
-    </p>
+    <p>최지이 | Ji Yee Choi | wldlglaso@naver.com</p>
+    <p>Copyright 2024. JiYee Choi. All rights reserved.</p>
   </section>
 );
 
