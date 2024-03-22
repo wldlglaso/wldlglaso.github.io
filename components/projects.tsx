837d6c141bca0d03fa6da9feeb9388908cc204b5
sujin <sujin3473@gmail.com>
2024-03-22 14:39:23 +0900

    add: Read More button

--- components/projects.tsx
+++ components/projects.tsx
@@ -12,7 +12,7 @@ const Projects = () => (
 );
 
 const Card = ({ title, description, image, url }: Project) => (
-  <div className="flex flex-col md:flex-row gap-x-[28px]">
+  <div className="flex flex-col justify-between md:flex-row gap-x-[28px]">
     <Image
       src={image}
       alt={title}
@@ -20,16 +20,16 @@ const Card = ({ title, description, image, url }: Project) => (
       height={160}
       className="shrink-0 mx-auto"
     />
-    <div className="flex flex-col justify-between py-[8px] mt-[10px] md:mt-0">
+    <div className="flex flex-col justify-between py-[8px] md:mt-0">
       <div>
         <p className="text-[20px] font-bold">{title}</p>
         <p className="text-[16px] mt-[16px]">{description}</p>
       </div>
-      <Link href={url} className="flex items-center mt-[10px] md:mt-0">
-        <span className="mr-[4px] text-[14px] font-bold text-blue-main">
+      <Link href={url} className="flex items-center mt-[10px] md:mt-[20px]">
+        <span className="mr-[4px] text-[14px] font-bold text-gray-A">
           Read more
         </span>
-        <Chevron />
+        <Chevron className="fill-gray-A" />
       </Link>
     </div>
   </div>
